22cbdfc35a5b4ac581c82959c76d51bd46197ef5
MateMar04 <mateo.marchisone@gmail.com>
2021-10-20 21:32:40 -0300

    draw table correctly

--- script.js
+++ script.js
@@ -2,21 +2,15 @@ var nroDeOrden;
 var nombre;
 var precio;
 
-const lista = new Array();
-
-
-
 function enviar() {
     nroDeOrden = document.getElementById("orden").value;
     nombre = document.getElementById("nombre").value;
     precio = document.getElementById("precio").value;
 }
 
-
-
-function imprimir() {
+function ensureSize(size) {
     var table = document.getElementById("tabla");
-    for (var i = 1; i < nroDeOrden; i++) {
+    for (var i = size; i <= nroDeOrden; i++) {
         var row = table.insertRow(i);
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
@@ -25,12 +19,16 @@ function imprimir() {
         cell2.innerHTML = "0";
         cell3.innerHTML = "0";
     }
+}
 
-    var lastRow = table.insertRow(nroDeOrden)
-    var cellOrden = lastRow.insertCell(0);
-    var cellNombre = lastRow.insertCell(1);
-    var cellPrecio = lastRow.insertCell(2);
-    cellOrden.innerHTML = nroDeOrden;
-    cellNombre.innerHTML = nombre;
-    cellPrecio.innerHTML = precio;
+function imprimir() {
+    var table = document.getElementById("tabla");
+    let rows = table.rows;
+
+    ensureSize(rows.length);
+
+    var modifyRow = rows[nroDeOrden]
+    var cellOrden = modifyRow.cells[0].innerHTML = nroDeOrden;
+    var cellNombre = modifyRow.cells[1].innerHTML = nombre;
+    var cellPrecio = modifyRow.cells[2].innerHTML = precio;
 }
